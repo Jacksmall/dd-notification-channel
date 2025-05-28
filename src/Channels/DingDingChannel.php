2aea8b19c0e4f56bfa27d09e00f990c0afe68a13
chenkuan <812254315@qq.com>
2025-05-28 15:00:21 +0800

    init

--- src/Channels/DingDingChannel.php
+++ src/Channels/DingDingChannel.php
@@ -5,6 +5,7 @@ namespace Jacksmall\DdNotificationChannel\Channels;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Notifications\Notification;
+use Jacksmall\DdNotificationChannel\Messages\DingDingMessage;
 use Psr\Http\Message\ResponseInterface;
 
 class DingDingChannel
@@ -31,6 +32,29 @@ class DingDingChannel
             return;
         }
 
-        return $this->http->post($url, $notification->toDingDing($notifiable));
+        return $this->http->post($url, $this->buildJsonPayload(
+            $notification->toDingDing($notifiable)
+        ));
+    }
+
+    /**
+     * @param DingDingMessage $message
+     * @return array
+     */
+    protected function buildJsonPayload(DingDingMessage $message)
+    {
+        $optionalFields = array_filter([
+            'content' => data_get($message, 'content'),
+            'atMobiles' => data_get($message, 'atMobiles'),
+            'atUserIds' => data_get($message, 'atUserIds'),
+            'isAtAll' => data_get($message, 'isAtAll'),
+            'title' => data_get($message, 'title'),
+            'text' => data_get($message, 'text'),
+            'messageUrl' => data_get($message, 'messageUrl'),
+            'picUrl' => data_get($message, 'picUrl'),
+            'singleTitle' => data_get($message, 'singleTitle'),
+            'btnOrientation' => data_get($message, 'btnOrientation'),
+        ]);
+        return [$optionalFields];
     }
 }
